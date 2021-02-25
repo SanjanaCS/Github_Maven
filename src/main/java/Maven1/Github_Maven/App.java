1aa0d2e5ea31652745d278c8ef9668ba836df061
SanjanaCS <sanjanacashikar98@gmail.com>
2021-02-25 04:22:05 +0000

    Github_Maven

---
+++ src/main/java/Maven1/Github_Maven/App.java
@@ -0,0 +1,13 @@
+package Maven1.Github_Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/test/testcase1.java
@@ -0,0 +1,64 @@
+package test;
+
+ 
+
+import org.testng.annotations.Test;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+ 
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+ 
+
+import pages.Logintest;
+
+ 
+
+ 
+
+public class testcase1 {
+WebDriver driver;
+XSSFWorkbook wb;
+XSSFSheet sheet;
+ 
+
+@BeforeMethod
+public void launchBrowser() {
+	System.setProperty("webdriver.chrome.driver","chromedriver");
+	
+	System.setProperty("webdriver.gecko.driver","geckodriver");	
+	driver = new ChromeDriver();
+	driver.get("https://github.com/");
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(3000,TimeUnit.MILLISECONDS);
+}
+
+@Test
+public void Logintest() throws IOException {
+	FileInputStream file = new FileInputStream("Testdata.xlsx");
+    wb = new XSSFWorkbook(file);
+    sheet = wb.getSheet("datasheet");
+    String UserName = sheet.getRow(1).getCell(0).getStringCellValue();
+    String Password = sheet.getRow(1).getCell(0).getStringCellValue();
+	Logintest obj = new Logintest(driver);
+	obj.Login(UserName,Password);
+}
+
+@AfterMethod
+public void CloseBrowser() {
+	driver.quit();
+}
+
+ 
+
+}
